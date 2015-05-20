1ea285e9662802b07b8d2b3a6f7bbb57d2a7c9a0
faridjammal <faridjammal@gmail.com>
2015-05-20 16:36:39 +0200

    L'interface

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Salle.java
@@ -0,0 +1,30 @@
+package com.mycompany.mavenproject1;
+
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Farid
+ */
+public class Salle {
+ 
+    private int id_salle;
+    private int id_locale;
+    static int nb_equipement;
+    String x;
+   // private list <string> listes_salles = newArrayList<string>;
+   // private list <equipement> listes_equipement = newArrayList<string>;
+   private ArrayList<String> listes_salles = new ArrayList<String>();
+   private ArrayList<String> listes_equipement = new ArrayList<String>();
+   
+   
+     
+}
